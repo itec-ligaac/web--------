effbdb0b557d64b1313ad6afe6692ac6bea5285b
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2021-03-27 20:18:54 +0200

    Added searchByCoordinates

--- backend/controllers/hotel.controller.js
+++ backend/controllers/hotel.controller.js
@@ -1,5 +1,8 @@
 const { StatusCodes } = require("http-status-codes");
 const Amadeus = require('amadeus');
+const axios = require('axios')
+const qs = require('qs');
+const { func } = require("joi");
 
 var amadeus = new Amadeus({
     clientId: process.env.HOTEL_API_KEY,
@@ -30,10 +33,40 @@ const searchByLocation = async (req, res) => {
 
 const searchByCoordinates = async (req, res) => {
     try {
-        res.status(StatusCodes.CREATED).json({
-            success: false,
-            reason: "Not yet implemented - sdk doensn't offer support for coords, use basic api calls"
+        const { latitude, longitude } = req.body;
+        data = {
+            grant_type: 'client_credentials',
+            client_id: process.env.HOTEL_API_KEY,
+            client_secret: process.env.HOTEL_API_SECRET
+        }
+        data = qs.stringify(data)
+        const options = {
+            method: 'POST',
+            headers: { 'content-type': 'application/x-www-form-urlencoded' },
+            data,
+            url: 'https://test.api.amadeus.com/v1/security/oauth2/token',
+          };
+        axios(options).then(function (response) {
+              accessToken = response.data.access_token
+              const hotelOptions = {
+                url: `https://test.api.amadeus.com/v2/shopping/hotel-offers?latitude=${latitude}&longitude=${longitude}&radius=50`,
+                type: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`
+                },
+              }
+              axios(hotelOptions).then(function (params) {
+                    res.status(StatusCodes.CREATED).json({
+                        success: true,
+                        data: params.data
+                    });
+              }).catch(function (err){
+                console.log(err);
+            });
+        }).catch(function (err){
+            console.log(err);
         });
+        
     }
     catch (error) {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
